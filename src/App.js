c7659cc27f983676df59bfa2c6072a2c600aa580
Mehul1604 <mathurmehul3@gmail.com>
2020-07-14 00:35:12 +0530

    directory structure made

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React , {Component} from 'react';
+import {BrowserRouter , Route , Switch} from 'react-router-dom';
 
+import 'bootstrap/dist/css/bootstrap.css';
+//@ts-ignore
+import 'bootstrap/dist/js/bootstrap';
+// to import react-bootstrap import {...} from 'react-bootstrap'
+import 'jquery';
+import 'popper.js';
+import Login from './components/Login';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>THIS IS NOT REDDIT</h1>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Switch>
+        <Route path="/" component={Home} exact/>
+        <Route path="/login" component={Login} exact/>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
